1049f7abd66be81f2bb75ab3e3efc7aba9599ff5
Erik4421 <134088430+Erik4421@users.noreply.github.com>
2023-05-24 01:26:48 +0000

    projeto-aula

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Erik de Oliveira",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "oliveiraerik464@gmail.com",
+  gitHub: "Erik4421",
+  instagram: "erikdeoliveiras",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
